389e7032ad13160c79492f049f38157fb07aad93
Dan Kolbman <dankolbman@gmail.com>
2015-04-19 07:34:11 -0400

    Add source

---
+++ facts.js
@@ -0,0 +1,62 @@
+var UI = require('ui');
+var ajax = require('ajax');
+
+// Create a Card with title and subtitle
+var card = new UI.Card({
+  title:'Brick Facts',
+  subtitle:'Fetching...'
+});
+
+// Display the Card
+card.show();
+
+// Construct URL
+var URL = 'http://balmy-channel-92009.appspot.com/api';
+
+// Make the request
+ajax(
+  {
+    url: URL,
+    type: 'json'
+  },
+  function(data) {
+    // Success!
+    console.log("Successfully fetched fact");
+
+    // Extract data
+    var fact = data.fact;
+
+    card.title('Brick Fact:');
+    card.subtitle('');
+    card.body(fact);
+
+  },
+  function(error) {
+    // Failure!
+    console.log('Failed fetching fact: ' + error);
+  }
+);
+
+card.on('click', function(e) {
+  ajax(
+  {
+    url: URL,
+    type: 'json'
+  },
+  function(data) {
+    // Success!
+    console.log("Successfully fetched fact");
+
+    // Extract data
+    var fact = data.fact;
+
+    card.title('Brick Fact:');
+    card.subtitle('');
+    card.body(fact);
+
+  },
+  function(error) {
+    // Failure!
+    console.log('Failed fetching fact: ' + error);
+  }
+);
